9251171e6ec18e059e65fcc1cfaaf87cea60a986
yuuki <f-1-clover4.saku@i.softbank.jp>
2024-01-18 11:38:28 +0900

    :sparkles: Feat:　新規会員登録画面作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  skip_before_action :require_login
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      auto_login(@user)
+      redirect_to home_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
