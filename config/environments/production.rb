6f745fcbf5fe89a8b38d8e97719a7b18a41bd60f
yuuki <f-1-clover4.saku@i.softbank.jp>
2024-02-03 16:41:13 +0900

    :sparkles: Feat: メール送信機能

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,15 +65,18 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = Settings.default_url_options.to_h
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    domain: 'gmail.com',
-    port: 587,
-    user_name: Rails.application.credentials.gmail[:user_name],
-    password: Rails.application.credentials.gmail[:password],
-    authentication: :login,
-    enable_starttls_auto: true
-  }
+  if Rails.application.credentials.gmail
+    config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      domain: 'gmail.com',
+      port: 587,
+      user_name: Rails.application.credentials.gmail[:user_name],
+      password: Rails.application.credentials.gmail[:password],
+      authentication: :login,
+      enable_starttls_auto: true
+    }
+  end
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
