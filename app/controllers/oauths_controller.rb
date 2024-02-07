cb7d6fe92c2e0637880e6e2638f325172877ee7a
yuuki <f-1-clover4.saku@i.softbank.jp>
2024-02-07 14:17:18 +0900

    :sparkles: Feat: google認証機能

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -8,11 +8,11 @@ class OauthsController < ApplicationController
   def callback
     provider = auth_params[:provider]
     if (@user = login_from(provider))
-      redirect_to root_path, notice:"#{provider.titleize}アカウントでログインしました"
+      redirect_to home_path, notice:"#{provider.titleize}アカウントでログインしました"
     else
       begin
         signup_and_login(provider)
-        redirect_to root_path, notice:"#{provider.titleize}アカウントでログインしました"
+        redirect_to home_path, notice:"#{provider.titleize}アカウントでログインしました"
       rescue
         redirect_to root_path, alert:"#{provider.titleize}アカウントでのログインに失敗しました"
       end
