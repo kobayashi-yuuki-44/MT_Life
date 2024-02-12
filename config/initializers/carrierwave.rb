32563a9e53e5c362f7a0538c46d2dff7773503c3
yuuki <f-1-clover4.saku@i.softbank.jp>
2024-02-12 18:35:40 +0900

    :sparkles: Feat: profile機能

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 CarrierWave.configure do |config|
-    if Rails.env.production?
-      config.fog_provider = 'fog/aws'
-      config.fog_credentials = {
-        provider:              'AWS',
-        aws_access_key_id:     'AWS_ACCESS_KEY_ID',
-        aws_secret_access_key: 'AWS_SECRET_ACCESS_KEY',
-        region:                'AWS_REGION',
-      }
-      config.fog_directory  = 'NAME_OF_BUCKET'
-    end
+  if Rails.env.production?
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region:                ENV['AWS_REGION'],
+    }
+    config.fog_directory  = ENV['NAME_OF_BUCKET']
   end
+end
