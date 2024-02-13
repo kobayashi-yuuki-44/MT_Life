b06742bfa8ddaa9dbf0798db86bb3ef35e3c93b2
yuuki <f-1-clover4.saku@i.softbank.jp>
2024-02-13 17:32:14 +0900

    :sparkles: Feat: google認証機能

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,14 @@
 CarrierWave.configure do |config|
-  
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region:                ENV['AWS_REGION'],
-  }
-  config.fog_directory  = ENV['NAME_OF_BUCKET']
-  config.fog_public = false
-  config.fog_attributes = {}
+  if Rails.env.production?
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region:                ENV['AWS_REGION'],
+    }
+    config.fog_directory  = ENV['NAME_OF_BUCKET']
+    config.fog_public = false
+    config.fog_attributes = {}
+  end
 end
