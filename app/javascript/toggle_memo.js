3d657d1f594362636de21bd2e2fad4658108cd84
yuuki <f-1-clover4.saku@i.softbank.jp>
2024-03-19 16:35:59 +0900

    :sparkles: Feat: メモ編集

--- app/javascript/toggle_memo.js
+++ app/javascript/toggle_memo.js
@@ -3,6 +3,7 @@ document.addEventListener("turbo:load", function() {
   console.log("turbo:load event triggered");
   const toggleButton = document.getElementById("toggle-memo");
   const memoContent = document.getElementById("content");
+  const editableMemo = document.getElementById("editable-memo");
 
   console.log({ toggleButton, memoContent });
 
@@ -12,6 +13,33 @@ document.addEventListener("turbo:load", function() {
       console.log("Toggle button clicked");
       memoContent.style.display = memoContent.style.display === "none" ? "block" : "none";
       toggleButton.textContent = memoContent.style.display === "block" ? "メモを隠す" : "メモを見る";
+    });
+  }
+  
+  if (editableMemo) {
+    const questionId = editableMemo.getAttribute('data-question-id');
+    const memoId = editableMemo.getAttribute('data-memo-id');
+  
+    editableMemo.addEventListener('blur', function() {
+      const content = editableMemo.innerText.trim();
+      console.log("Edited content:", content);
+      
+      const memoUpdateUrl = `/questions/${questionId}/memos/${memoId}`;
+      fetch(memoUpdateUrl, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRF-Token': document.querySelector('[name=csrf-token]').content
+        },
+        body: JSON.stringify({ memo: { content: content } })
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
     });
   }
 });
